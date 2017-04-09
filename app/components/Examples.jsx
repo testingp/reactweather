bad9a31eff9e0b5bc7667788ef92781ff677fc22
Josip Susic <josip.susic@hotmail.com>
2017-04-09 16:11:20 +0200

    inital commit

---
+++ app/components/Examples.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var { Link } = require('react-router');
+
+var Examples = (props) => {
+    return (
+        <h2>Examples</h2>
+    );
+}
+
+module.exports = Examples;
